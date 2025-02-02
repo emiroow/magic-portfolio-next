79fa46258679fc4ccc5842397924e9a2c9fbeb4c
emiroow <amir_esmaeelzadeh@yahoo.com>
2025-02-03 01:15:34 +0330

    push

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -2,5 +2,5 @@ import { useTranslations } from "next-intl";
 
 export default function NotFoundPage() {
   const t = useTranslations("NotFoundPage");
-  return <h1 className="text-red-500">{t("title")}</h1>;
+  return <h1>{t("title")}</h1>;
 }
