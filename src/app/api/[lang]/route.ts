a41b22efa5d87ace11348e45a8c907fd954b76c2
emiroow <amir_esmaeelzadeh@yahoo.com>
2025-02-03 01:15:20 +0330

    create routes

--- src/app/api/[lang]/route.ts
+++ src/app/api/[lang]/route.ts
@@ -1,5 +1,30 @@
-import { NextResponse } from "next/server";
+import { connectDB } from "@/config/dbConnection";
+import { educationModel } from "@/models/education";
+import { socialModel } from "@/models/social";
+import { userModel } from "@/models/user";
+import { workModel } from "@/models/work";
+import { NextRequest, NextResponse } from "next/server";
+export const GET = async (
+  request: NextRequest,
+  { params }: { params: Promise<{ lang: string }> }
+) => {
+  const lang = (await params).lang;
+  await connectDB();
+  try {
+    const user = await userModel.findOne({ lang });
+    const education = await educationModel.find({ lang });
+    const work = await workModel.find({ lang });
+    const social = await socialModel.find();
 
-export async function GET() {
-  return NextResponse.json({ message: "get data" }, { status: 200 });
-}
+    return NextResponse.json(
+      { user, education, work, social },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+};
