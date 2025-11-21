4817adcca7242a743853b7f2e557b0e71878d8cb
emiroow <amir_esmaeelzadeh@yahoo.com>
2025-11-21 17:27:20 +0330

    refactor: reorganize image domains and remote patterns for improved readability

--- next.config.mjs
+++ next.config.mjs
@@ -7,18 +7,23 @@ const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["api.emiroow.ir", "localhost", "127.0.0.1", "your-vercel-blob-domain", siteUrl],
+    domains: [
+      "api.emiroow.ir",
+      "localhost",
+      "127.0.0.1",
+      "your-vercel-blob-domain",
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.public.blob.vercel-storage.com",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
   },
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "**.public.blob.vercel-storage.com",
-    },
-    {
-      protocol: "http",
-      hostname: "localhost",
-    }
-  ],
 };
 
 export default withNextIntl(nextConfig);
