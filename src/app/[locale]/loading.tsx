331223471cdb6cb5b11cc4c76fcc3053d63df627
Amir esmaeilzadeh <86824829+emiroow@users.noreply.github.com>
2026-06-29 14:14:15 +0330

    fix loading

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,10 +1,10 @@
 const Loading = () => {
   return (
-    <div className="w-full h-[80vh]  flex justify-center items-center">
+    <div className="w-full h-[100vh] flex justify-center items-center">
       <div role="status">
         <svg
           aria-hidden="true"
-          className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-black dark:fill-white"
+          className="w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-black dark:fill-white"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
